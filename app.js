79cf121e5e0829ecb884288cbe69382728c5f7fb
Kelly <kellypitts91@yahoo.com>
2017-11-24 13:52:02 +1300

    add html page

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var connector = new builder.ChatConnector({
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
+server.get(/.*/, restify.serveStatic({ 'directory': '.', 'default': 'index.html' }));
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
