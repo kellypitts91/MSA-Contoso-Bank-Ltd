94c749f620014c9dfa4301bf212d5519c838c84f
Kelly <kellypitts91@yahoo.com>
2017-11-25 21:23:40 +1300

    testing bot

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    //appId: "4c694950-0731-499a-b94d-1b2c3c6e9f57",
-    //appPassword: "bpdeKIR8849@@hesOZZY1}%"
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "4c694950-0731-499a-b94d-1b2c3c6e9f57",
+    appPassword: "bpdeKIR8849@@hesOZZY1}%"
+    // appId: process.env.MICROSOFT_APP_ID,
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users 
